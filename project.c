07844c6b75be844a59907c3060bf44ac864f613b
ryoniyokita <107526934+ryoniyokita@users.noreply.github.com>
2022-06-22 10:31:25 +0800

    Add files via upload

---
+++ project.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (){
+	
+	int a, b, c;
+	
+	int hasil;
+	
+	printf("masukkan nilai a: ");
+	scanf("%d", &a);
+	printf("masukkan nilai b: ");
+	scanf("%d", &b);
+	printf("masukkan nilai c: ");
+	scanf("%d", &c);
+	hasil=(a+b+c)/3;
+	printf("\n hasil nilai= %d", hasil);
+	
+	float jari, phi;
+	
+	printf("masukkan jari jari :");
+	scanf("%f", &jari);
+	printf("masukkan jari jari :");
+	scanf("%f", &jari);
+	printf("luas phi :");
+	scanf("%f", &phi);
+	hasil=(phi*jari*jari);
+	printf("\n hasil nilai= %f", hasil);
+	
+	
+	;return 0;
+}
